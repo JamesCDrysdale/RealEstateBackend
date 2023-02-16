5f1dd716159cbda2980ee3bdbd0c8d2039ce1ab5
James Drysdale <james.drysdale@codeclan.com>
2023-02-16 08:03:35 +0000

    Create property done in a session :card_file_box:

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Property from '../mongodb/models/property.js';
 import User from '../mongodb/models/user.js';
 
@@ -5,7 +6,14 @@ const getAllProperties = async (req, res) => {};
 
 const getPropertyDetail = async (req, res) => {};
 
-const createProperty = async (req, res) => {};
+const createProperty = async (req, res) => {
+    const { title, description, propertyType, location, price, photo, email } = req.body;
+
+    //Start a new session - makes property atomic
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+};
 
 const updateProperty = async (req, res) => {};
 
