653e6f86a900f0279ac2ab528d876922c126f268
James Drysdale <james.drysdale@codeclan.com>
2023-02-16 02:31:14 +0000

    Users are added to db appropriately :sparkle:

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,27 @@ const getAllUsers = async (req, res) => {
 
 };
 
-const createUser = async (req, res) => {};
+const createUser = async (req, res) => {
+    try {
+        const { name, email, avatar } = req.body;
+
+        // Check user exists
+        const userExists = await User.findOne({ email });
+
+        if (userExists) return res.status(200).json(userExists);
+
+        // Create new user in DB if they are not there
+        const newUser = await User.create({
+            name,
+            email,
+            avatar
+        })
+
+        res.status(200).json(newUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }   
+};
 
 const getUserInfoByID = async (req, res) => {};
 
